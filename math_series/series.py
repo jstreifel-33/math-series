b04aec3fb1ac1f93acdff9623c9c7685cb605b27
Joe Streifel <josephstreifel@gmail.com>
2021-11-30 17:16:44 -0800

    changes tab spacing for linter

--- math_series/series.py
+++ math_series/series.py
@@ -1,37 +1,39 @@
 def fibonacci(n):
-  """
-  Returns 'nth' number in the Fibonacci
-  sequence. (0, 1, 1, 2, 3, 5, ...)
-  """
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
-  
-  return fibonacci(n-1) + fibonacci(n-2)
+    """
+    Returns 'nth' number in the Fibonacci
+    sequence. (0, 1, 1, 2, 3, 5, ...)
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 def lucas(n):
-  """
-  Returns 'nth' number in the Lucas sequence.
-  Similar to Fibonacci, but starts with 2 and 1.
-  (2, 1, 3, 4, 7, 11, 18, ...)
-  """
-  if n == 0:
-    return 2
-  elif n == 1:
-    return 1
-  
-  return lucas(n-1) + lucas(n-2)
+    """
+    Returns 'nth' number in the Lucas sequence.
+    Similar to Fibonacci, but starts with 2 and 1.
+    (2, 1, 3, 4, 7, 11, 18, ...)
+    """
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+
+    return lucas(n - 1) + lucas(n - 2)
+
 
 def sum_series(n, i=0, j=1):
-  """
-  Returns 'nth' number in any sum sequence,
-  given a first and second argument.
-  (i, j, i+j, i+2j, 2i+3j, ...)
-  """
-  if n == 0:
-    return i
-  elif n == 1:
-    return j
-  
-  return sum_series(n-1, i, j) + sum_series(n-2, i, j)
+    """
+    Returns 'nth' number in any sum sequence,
+    given a first and second argument.
+    (i, j, i+j, i+2j, 2i+3j, ...)
+    """
+    if n == 0:
+        return i
+    elif n == 1:
+        return j
+
+    return sum_series(n - 1, i, j) + sum_series(n - 2, i, j)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,96 +1,117 @@
 from math_series.series import fibonacci, lucas, sum_series
 import pytest
 
-#Deco for skipping tests:
-#@pytest.mark.skip("pending")
+# Deco for skipping tests:
+# @pytest.mark.skip("pending")
 
 # fibonacci tests
 
 ## proof of import life
 
+
 def test_import_fibonacci():
-  assert fibonacci
+    assert fibonacci
+
 
 ## signature testing
 
+
 def test_fibonacci_0():
-  actual = fibonacci(0)
-  expected = 0
-  assert actual == expected
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
 
 def test_fibonacci_2():
-  actual = fibonacci(2)
-  expected = 1
-  assert actual == expected
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
 
 def test_fibonacci_5():
-  actual = fibonacci(5)
-  expected = 5
-  assert actual == expected
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
+
 
 def test_fibonacci_10():
-  actual = fibonacci(10)
-  expected = 55
-  assert actual == expected
+    actual = fibonacci(10)
+    expected = 55
+    assert actual == expected
+
 
 # lucas tests
 
 ## proof of import life
 
+
 def test_import_lucas():
-  assert lucas
+    assert lucas
+
 
 ## signature testing
 
+
 def test_lucas_0():
-  actual = lucas(0)
-  expected = 2
-  assert actual == expected
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
 
 def test_lucas_1():
-  actual = lucas(1)
-  expected = 1
-  assert actual == expected
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
 
 def test_lucas_5():
-  actual = lucas(5)
-  expected = 11
-  assert actual == expected
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+
 
 def test_lucas_10():
-  actual = lucas(10)
-  expected = 123
-  assert actual == expected
+    actual = lucas(10)
+    expected = 123
+    assert actual == expected
+
 
 # sum_series tests
 
 ## proof of import life
 
+
 def test_import_sum_series():
-  assert sum_series
+    assert sum_series
+
 
 ## signature testing
 
 ### variable n; default i, j
 
+
 def test_sum_series_0():
-  actual = sum_series(0)
-  expected = 0
-  assert actual == expected
+    actual = sum_series(0)
+    expected = 0
+    assert actual == expected
+
 
 def test_sum_series_5():
-  actual = sum_series(5)
-  expected = 5
-  assert actual == expected
+    actual = sum_series(5)
+    expected = 5
+    assert actual == expected
+
 
 ### fixed n=5; variable i, j
 
+
 def test_sum_series_as_fib():
-  actual = sum_series(5, 0, 1)
-  expected = 5
-  assert actual == expected
+    actual = sum_series(5, 0, 1)
+    expected = 5
+    assert actual == expected
+
 
 def test_sum_series_as_lucas():
-  actual = sum_series(5, 2, 1)
-  expected = 11
-  assert actual == expected
+    actual = sum_series(5, 2, 1)
+    expected = 11
+    assert actual == expected
