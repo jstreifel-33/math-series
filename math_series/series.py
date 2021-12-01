2bc0e1f1bd3924236132f5248ae29a9c72b4c774
Joe Streifel <josephstreifel@gmail.com>
2021-11-30 16:40:46 -0800

    updates comparators used to be ==, adds to sum_series

--- math_series/series.py
+++ math_series/series.py
@@ -3,9 +3,9 @@ def fibonacci(n):
   Returns 'nth' number in the Fibonacci
   sequence. (0, 1, 1, 2, 3, 5, ...)
   """
-  if n < 1:
+  if n == 0:
     return 0
-  elif n < 2:
+  elif n == 1:
     return 1
   
   return fibonacci(n-1) + fibonacci(n-2)
@@ -16,9 +16,9 @@ def lucas(n):
   Similar to Fibonacci, but starts with 2 and 1.
   (2, 1, 3, 4, 7, 11, 18, ...)
   """
-  if n < 1:
+  if n == 0:
     return 2
-  elif n < 2:
+  elif n == 1:
     return 1
   
   return lucas(n-1) + lucas(n-2)
@@ -29,9 +29,9 @@ def sum_series(n, i=0, j=1):
   given a first and second argument.
   (i, j, i+j, i+2j, 2i+3j, ...)
   """
-  if n < 1:
-    return 0
-  elif n < 2:
-    return 1
+  if n == 0:
+    return i
+  elif n == 1:
+    return j
   
   return sum_series(n-1) + sum_series(n-2)
